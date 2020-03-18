8525a5d10f4285199eb22e193c036b34fd2baf14
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-18 20:43:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,15 +7,23 @@ class Dog
 		@name = name
 		@@all << self
 	end
+	def self.all 
+	  @@all 
+	end 
 
-	def self.clear_all
-		@@all.clear
-
-	end
-
+	
 	def self.print_all
 		@@all.each do |dog|
 			puts dog.name
 		end
 	end
+	
+	def self.clear_all
+		@@all.clear
+
+	end
+	
+	def save
+	  @@all << self 
+	end 
 end
